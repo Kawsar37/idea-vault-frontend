46c4f1e999b9c7b32b955cee5b5271a7e6f7012c
Kawsar37 <kawsarali750@gmail.com>
2026-05-21 02:48:07 +0600

    force dynamic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import IdeaCard from "@/components/IdeaCard";
 import { IdeasMarquee } from "@/components/IdeaMarquee";
 import ImageHover from "@/components/ImageHover";
