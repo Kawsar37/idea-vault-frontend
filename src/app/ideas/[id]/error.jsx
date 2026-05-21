5bafd1335a51a41a4e473b2de49ce6e1d5051ae5
Kawsar37 <kawsarali750@gmail.com>
2026-05-21 17:26:24 +0600

    error.js added

---
+++ src/app/ideas/[id]/error.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+export default function error() {
+  return (
+    <div className="flex flex-col gap-6 items-center justify-center min-h-[70vh]">
+      <h1 className="text-(--p-color) text-6xl">404!</h1>
+      <h1 className="text-3xl">Something Went Wrong</h1>
+      <Link className="bg-amber-300 btn rounded-full" href={"/"}>
+        Back To Home
+      </Link>
+    </div>
+  );
+}
